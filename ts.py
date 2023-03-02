5667333b04708db5c4429bc6d1bd507e8126e4ca
fansirsqi <2104898527@qq.com>
2023-03-02 10:23:18 +0800

    尝试整理提交

---
+++ ts.py
@@ -0,0 +1,10 @@
+# _*_coding:utf-8_*_
+
+# @PROJECT : audio_tools
+# @Time : 2022/11/25 12:03
+# @Author : Byseven
+# @File : ts.py
+# @SoftWare:
+from xmltool import *
+
+clen_mod_sounds(r'D:\senki-naruto\assets\Element\Pain', r'D:\senki-naruto\assets\Audio\Pain')
